231682fc76735c39542b1bd5ed7730237cd35534
Anna Baik <annaheyonbaik@gmail.com>
2019-12-08 21:43:34 +0000

    Initial commit - create first template
    Following CodeSchool/Pluralsight Try Django - this is up and including "The Template of Data" section of the course.

---
+++ main_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', {'treasures' : treasures})
+
+class Treasure:
+    def __init__(self, name, value, material, location):
+        self.name = name
+        self.value = value
+        self.material = material
+        self.location = location
+
+treasures = [
+    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM"),
+    Treasure("Fool's Gold", 0, 'oyrite', "Fool's Falls, CO"),
+    Treasure('Coffee Can', 20.00, 'tin', 'Acme, CA'),
+]
