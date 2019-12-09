13db0d1cddef831237d60e3d324786a7922e5c44
Anna Baik <annaheyonbaik@gmail.com>
2019-12-09 23:05:43 +0000

    Add table
    Added table to organise page. Note that until the images are added, the table rows don't line up properly.
    Used placekitten as placeholder for images as the original images don't seem to be on the Codeschool Github.

--- main_app/views.py
+++ main_app/views.py
@@ -5,14 +5,15 @@ def index(request):
     return render(request, 'index.html', {'treasures' : treasures})
 
 class Treasure:
-    def __init__(self, name, value, material, location):
+    def __init__(self, name, value, material, location, img_url):
         self.name = name
         self.value = value
         self.material = material
         self.location = location
+        self.img_url = img_url
 
 treasures = [
-    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM"),
-    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO"),
-    Treasure('Coffee Can', 20.00, 'tin', 'Acme, CA'),
+    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM",'https://placekitten.com/150/100'),
+    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO",'https://placekitten.com/150/100'),
+    Treasure('Coffee Can', 20.00, 'tin', 'Acme, CA','https://placekitten.com/150/100'),
 ]
